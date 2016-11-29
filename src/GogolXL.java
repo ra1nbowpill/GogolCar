1a4c460bed753b74453517f7ece1e12cdc05c992
ra1nbowpill <ygor.gallina@gmail.com>
2016-11-29 09:31:46 +0100

    Maj GogolXL

--- src/GogolXL.java
+++ src/GogolXL.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class GogolXL implements Algo {	
+public class GogolXL {
 	/*
 	Dans le cas général, il y a toujours un nombre pair de sommets de degré impair.
 	La solution optimale peut être obtenue par l'algorithme suivant :
@@ -23,26 +23,36 @@ public class GogolXL implements Algo {
 	G les arêtes du plus court chemin de u à v.
 
 	 */
-	 
+
+	private Set<Element> oddVertices(IGraph graph) {
+		Set<Element> oddVertices = new TreeSet<>();
+		for (Element vertex : graph.getVertices()) {
+			if (graph.neighbours_out(vertex).size() % 2 == 1) {
+				oddVertices.add(vertex);
+			}
+		}
+		return oddVertices;
+	}
+
 	/**
 	 * Computes G' the complete graph containing the odd degree vertices of G
 	 * the weigh is the shortest path from x to y in G
-	 * @param the base graph
+	 * @param graph the base graph
 	 * @return a graph
 	 */
 	public IGraph constructOddGraph(IGraph graph) {
 		
 		IGraph oddGraph = new AdjacencyListGraph(Collections.emptySet());
-		Set<Integer> oddVertices = IGraph.oddVertices(graph);
+		Set<Element> oddVertices = oddVertices(graph);
 		
-		for (Integer oddVertex1 : oddVertices) {
-			for (Integer oddVertex2 : oddVertices) {
+		for (Element oddVertex1 : oddVertices) {
+			for (Element oddVertex2 : oddVertices) {
 				if (oddVertex1 == oddVertex2) {
 					continue;
 				}
 				// TODO this must save the given arc
-				oddGraph.addArc(new WeigthedArc<Integer>(oddVertex1, oddVertex2,
-						Dijkstra.dijkstra(graph, oddVertex1, oddVertex2).size()));
+				/*oddGraph.addArc(new WeighedArc(oddVertex1, oddVertex2, "",
+						Dijkstra.dijkstra(graph, oddVertex1, oddVertex2).size()));*/
 			}
 		}
 		
@@ -51,26 +61,21 @@ public class GogolXL implements Algo {
 	
 	public Set<Arc> perfectMatch(IGraph graph) {
 		Set<Arc> match = new TreeSet<>();
-		List<WeigthedArc<Integer>> arcs = new ArrayList<>();
-		Set<Integer> inMatch = new TreeSet<>();
+		List<WeighedArc> arcs = new ArrayList<>();
+		Set<Element> inMatch = new TreeSet<>();
 		
 		/* Getting all the arcs from the graph */
-		for (Integer vertex : graph.getVertices()) {
+		for (Element vertex : graph.getVertices()) {
 			// TODO	public List<Arc> IGraph::arcs_out(Integer vertex);
-			arcs.addAll(graph.arcs_out(vertex));
+			//arcs.addAll(graph.arcs_out(vertex));
 		}
 		
 		/* Sorting the arcs based on their weight */
-		Comparator<WeigthedArc<Integer>> c = new Comparator<WeigthedArc<Integer>>() {
-			@Override
-			public int compare(WeigthedArc<Integer> o1, WeigthedArc<Integer> o2) {
-				return o1.weight() - o2.weight();
-			}
-		};
-		arcs.sort(c);
-		
+
+		arcs.sort((o1, o2) -> o1.weight() - o2.weight());
+
 		/* Finding perfectMatch with euristic */
-		for (WeigthedArc<Integer> arc : arcs) {
+		for (WeighedArc arc : arcs) {
 			if (inMatch.contains(arc.src()) || inMatch.contains(arc.dst())) {
 				continue;
 			}
@@ -87,10 +92,10 @@ public class GogolXL implements Algo {
 		return match;
 	}
 	
-	private List<Arc> arcsOfPath(List<Integer> path) {
+	private List<Arc> arcsOfPath(List<Element> path) {
 		List<Arc> arcs = new ArrayList<>();
 		for (int i = 1; i < path.size(); i++) {
-			arcs.add(new Arc(path.get(i-1), path.get(i)));
+			arcs.add(new Arc(path.get(i-1), path.get(i), ""));
 		}
 		return arcs;
 	}
@@ -98,10 +103,11 @@ public class GogolXL implements Algo {
 	public IGraph makeGraphEulerian(IGraph graph, Set<Arc> match) {
 		
 		IGraph eulerGraph = new AdjacencyListGraph(graph);
-		
+		Dijkstra dijkstra = new Dijkstra(graph);
+
 		for (Arc matchArc : match) {
 			List<Arc> path = arcsOfPath(
-						Dijkstra.dijkstra(graph, matchArc.src(), matchArc.dst()) );
+						dijkstra.dijkstra(matchArc.src(), matchArc.dst()) );
 			for (Arc arc : path) {
 				// TODO This must allow multiArcs
 				eulerGraph.addArc(arc);
@@ -112,13 +118,13 @@ public class GogolXL implements Algo {
 		return null;
 	}
 	
-	public List<Integer> algo(IGraph graph, int root) {
+	public List<Element> algo(IGraph graph, Element root) {
 		
 		IGraph oddGraph = constructOddGraph(graph);
 		
 		Set<Arc> match = perfectMatch(oddGraph);
 		
-		IGraph eulerianGraph = makeGraphEulerian(graph, match);
+		IGraph eulerianGraph = makeGraphEulerian(oddGraph, match);
 		
 		GogolL a = new GogolL();
 		return a.algo(eulerianGraph, root);
